66a67b3e1ddb5283fbc1e2625c48753d6465347b
Oscar Fonts <oscar.fonts@gmail.com>
2014-08-01 17:50:19 +0200

    Add map decorations

--- js/modules/flyer.js
+++ js/modules/flyer.js
@@ -37,6 +37,31 @@ define(['leaflet', 'leaflet.layers', 'wms', 'leaflet-hash', 'css!flyer.css'], fu
 		}
 	}
 
+	// Add all sorts of decorations
+	L.control.scale().addTo(map);
+
+	var signature = L.control({position: "bottomright"});
+	signature.onAdd = function(map) {
+		var div = L.DomUtil.create("div", "leaflet-control-attribution");
+		div.innerHTML = '<div>Coded by <a href="http://fonts.cat" target="_blank">Oscar Fonts</a>, <a href="http://geomati.co" target="_blank">geomati.co</a>, 2014';
+        //div.style.backgroundColor = "rgba(255,255,255,0.7)";
+		//div.style.padding = "8px";
+		//div.style.fontSize = "11px";
+		//div.style.textAlign = "right";
+		//div.style.textDecoration = "none";
+		return div;
+	}
+	signature.addTo(map);
+
+	var logo = L.control({position: "bottomright"});
+	logo.onAdd = function(map) {
+		var div = L.DomUtil.create("div", "info legend");
+		div.innerHTML = '<img width="180" src="http://sorteny.fonts.cat/geoserver/www/logo.png">&nbsp;&nbsp;&nbsp;&nbsp;<img src="http://upload.wikimedia.org/wikipedia/commons/thumb/4/4b/MNarrow.svg/24px-MNarrow.svg.png">';
+        div.style.backgroundColor = "rgba(255,255,255,0.7)";
+		div.style.padding = "8px";
+		return div;
+	}
+	logo.addTo(map);
 
 });
 
