cdb0cc2c212e2847d058e842b4ffb25dfdbd820a
Marti Pericay <marti.pericay@gmail.com>
2015-03-05 17:48:01 +0100

    cookie management

--- js/modules/wms.js
+++ js/modules/wms.js
@@ -8,31 +8,49 @@ define(["http", "xml"], function(http, xml) {
             return bbox;
         },	    
 		service: function(url) {
-			/*function setCookie(user, pwd) {
+			function setCookie(user, pwd) {
 				var params = {
 						username: user,
 						password: pwd
 				}
  
-				http.get('http://maps.bgeo.es/geoserver/j_spring_security_check', params).then(function(response) {
-					//TODO: check that cookie is set
+				http.get('/geoserver/j_spring_security_check', params).then(function(response) {
+					//TODO: error mangement
+					//if(getCookie("SPRING_SECURITY_REMEMBER_ME_COOKIE") == "") alert("Wrong ID");
+					console.info("Cookie set");
 				});				
-			}*/
+			}
+			function getCookie(c_name) {
+			    if (document.cookie.length > 0) {
+			        c_start = document.cookie.indexOf(c_name + "=");
+			        if (c_start != -1) {
+			            c_start = c_start + c_name.length + 1;
+			            c_end = document.cookie.indexOf(";", c_start);
+			            if (c_end == -1) {
+			                c_end = document.cookie.length;
+			            }
+			            return unescape(document.cookie.substring(c_start, c_end));
+			        }
+			    }
+			    return "";
+			}
+			
 			function getCapabilities(user, pwd) {
 				var params = {
 					service: "wms",
 					version: "1.3.0",
 					request: "GetCapabilities"
 				}
+				
 				// If WMS address is absolute, proxify ... we'll use CORS!
 				/*if(url.indexOf("//") > -1) {
 					params.url = url;
 					url = "proxy/";
 				}*/
+				
 				if(user && pwd) http.auth.set(user, pwd);
 				return http.get(url, params).then(function(response) {
 				    if(!response) return false;
-				    //TODO: check if user is logged in looking for a Cookie!
 					return xml.read(response, true);
 				});				
 			}
@@ -43,7 +61,7 @@ define(["http", "xml"], function(http, xml) {
 				var layers = capabilities.WMS_Capabilities.Capability.Layer.Layer;
 				bbox = capabilities.WMS_Capabilities.Capability.Layer.BoundingBox;
 				//only one layer?
-				if(!layers.length) layers = new Array(layers);
+				if(layers && !layers.length) layers = new Array(layers);
 				for (var i in layers) {
 					ret.push({
 						title: layers[i].Title,
@@ -55,7 +73,7 @@ define(["http", "xml"], function(http, xml) {
 
 			return {
 				getLayers: function(user, pwd) {
-					//setCookie(user, pwd);
+					if(user && pwd) setCookie(user, pwd);
 					return getCapabilities(user, pwd).then(getLayers);
 				}
 			}
